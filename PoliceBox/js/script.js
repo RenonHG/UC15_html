b266f10216d1f5d6c0e8b45ef616aab0133585cb
RenonHG <renongusson@gmail.com>
2024-05-22 11:06:20 -0300

    JQuery

--- PoliceBox/js/script.js
+++ PoliceBox/js/script.js
@@ -2,37 +2,70 @@
 //let erick = "variavel para usar no contexto aplicado"
 //const gui = "variavel para usar fixamente (sem alteração)"
 
-let indexSlide = 0
+// let indexSlide = 0
+
+// function showSlide(index){
+//     const carrossel = document.querySelector(".carrossel");
+
+//     const totalSlide = 4
+    
+//     if(index >= totalSlide){
+//         indexSlide = 0
+//     } else if(index < 0){
+//         indexSlide = totalSlide - 1
+//     } else{
+//         indexSlide = index;
+//     }
+
+
+//     const offset = -indexSlide * 100
+//     console.log(offset)
+//     carrossel.style.transform = `translateX(${offset}%)`;
+// }
+
+// function anteriorSlide(){
+//     indexSlide = indexSlide - 1
+//     showSlide(indexSlide)
+// }
+
+// function proximoSlide(){
+//     indexSlide = indexSlide + 1
+//     showSlide(indexSlide)
+// }
+
+// setInterval(() => {
+//     proximoSlide()
+// }, 5000);
+
+let indexSlide = 0;
 
 function showSlide(index){
-    const carrossel = document.querySelector(".carrossel");
-
-    const totalSlide = 4
+    const carrossel = $(".carrossel");
+    const totalSlide = 4;
     
     if(index >= totalSlide){
-        indexSlide = 0
+        indexSlide = 0;
     } else if(index < 0){
-        indexSlide = totalSlide - 1
+        indexSlide = totalSlide - 1;
     } else{
         indexSlide = index;
     }
 
-
-    const offset = -indexSlide * 100
-    console.log(offset)
-    carrossel.style.transform = `translateX(${offset}%)`;
+    const offset = -indexSlide * 100;
+    console.log(offset);
+    carrossel.css("transform", `translateX(${offset}%)`);
 }
 
 function anteriorSlide(){
-    indexSlide = indexSlide - 1
-    showSlide(indexSlide)
+    indexSlide = indexSlide - 1;
+    showSlide(indexSlide);
 }
 
 function proximoSlide(){
-    indexSlide = indexSlide + 1
-    showSlide(indexSlide)
+    indexSlide = indexSlide + 1;
+    showSlide(indexSlide);
 }
 
 setInterval(() => {
-    proximoSlide()
+    proximoSlide();
 }, 5000);
